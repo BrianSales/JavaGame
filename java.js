5e0832bb95e58f0dd539fecb6acb2d3845a4bc90
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-02-26 11:02:15 -0600

    Added questions to my Javascript

---
+++ java.js
@@ -0,0 +1,120 @@
+//Create variables to house data
+var questionsArray = [
+    q1 = {
+      //question to be displayed
+      question:"What can you use a can of Mountain Dew to dissolve?",
+      //possible answers
+      correctAnswer:"A Mouse",
+      incorrectAnswers: {
+        choiceOne: "Gold",
+        choiceTwo: "A Tire",
+        choiceThree: "Credit Cards",
+      },
+      
+      
+    },
+    q2 = {
+      question:"Which of the following is a real measurement of weight",
+      correctAnswer:"Buttload",
+      incorrectAnswers: {
+        choiceOne: "Crap-Ton",
+        choiceTwo: "Eight-Ball",
+        choiceThree: "Onion",
+      },
+     
+    },
+    q3 = {
+      question:"What country apologizes so much that a 2009 Act known as 'The Apology Act' declared that an apology is not an admission of guilt",
+      correctAnswer:"Canada",
+      incorrectAnswers: {
+        choiceOne: "USA",
+        choiceTwo: "France",
+        choiceThree: "Austrialia",
+      },
+      
+    },
+    q4 = {
+      question:"What tree can produce more than one kind of fruit at a time?",
+      correctAnswer:"Fruit Salad Tree",
+      incorrectAnswers: {
+        choiceOne: "The Giving Tree",
+        choiceTwo: "Old Man Willow Tree",
+        choiceThree: "Groot",
+      },
+      
+    },
+    q5 = {
+      question:"What popular condiment was used as a medicinal product when it was first inroduced in the 1830s?",
+      correctAnswer:"Ketchup",
+      incorrectAnswers: {
+        choiceOne: "A1 Steak Sauce",
+        choiceTwo: "Mustard",
+        choiceThree: "McDonald's Szechuan Sauce",
+      },
+     
+    },
+    q6 = {
+      question:"What major US city was founded by a woamn?",
+      correctAnswer:"Miami",
+      incorrectAnswers: {
+        choiceOne: "Chicago",
+        choiceTwo: "Metropolis",
+        choiceThree: "New York",
+      },
+      
+    },
+    q7 = {
+      question:"In which wing of the White House is the oval office located?",
+      correctAnswer:"West Wing",
+      incorrectAnswers: {
+        choiceOne: "East Wing",
+        choiceTwo: "Southeast Wing",
+        choiceThree: "Northwest Wing",
+      },
+      
+    },
+    q8 = {
+      question:"The average human body contains how many pints of blood?",
+      correctAnswer:"9",
+      incorrectAnswers: {
+        choiceOne: "420",
+        choiceTwo: "5",
+        choiceThree: "360",
+      },
+      
+    },
+    q9 = {
+      question:"Who played Black Panther in the 2016 film of the same name?",
+      correctAnswer:"Chadwick Boseman",
+      incorrectAnswers: {
+        choiceOne: "Denzel Washington",
+        choiceTwo: "Brian Sales",
+        choiceThree: "",
+      },
+      
+    },
+    q10 = {
+      question:"The 2021 film Judas and the Black Messiah is a movie about what Chicago Revolutionary?",
+      correctAnswer:"Fred Hampton",
+      incorrectAnswers: {
+        choiceOne: "Jessie Jackson",
+        choiceTwo: "Bedford Moss",
+        choiceThree: "Alex Ortega",
+      },
+      
+    }
+
+    
+
+
+
+
+
+
+
+
+
+]
+
+
+    

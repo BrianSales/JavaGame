1bf6a4ce00ff1dcac22a2ba4faab531f9e9de87d
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-02-27 10:37:33 -0600

    added a timer to the game

--- java.js
+++ java.js
@@ -1,17 +1,31 @@
-//Create variables to house data
-
-
 const startButton = document.getElementById('start-btn');
 const questionContainerElement = document.getElementById('question-container');
 let shuffledQuestions, currentQuestionIndex
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
+console.log(answerButtonsElement)
+let counter = document.getElementById("clock")
 
 
 startButton.addEventListener('click', startGame);
 
+var timer = 30
+
+function countdown() {
+    setTimeout(function(){
+        
+        if (timer > 0){
+            timer--
+            console.log(counter)
+            counter.innerHTML = timer
+            countdown()
+        }
+    }, 1000
 
 
+
+    )
+}
 
 function startGame(){
     console.log('Started');
@@ -20,19 +34,45 @@ function startGame(){
     startButton.classList.add('hide');
     questionContainerElement.classList.remove('hide');
     setNextQuestion()
+    countdown()
+    
+    
+    
 
 }
 
+
+
+
+
+
+
+
+
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 
 }
 
 
 function setNextQuestion(){
+    
     showQuestion(shuffledQuestions[currentQuestionIndex])
 
 }
+
+
+
 
 
 function selectAnswer(){
@@ -41,13 +81,15 @@ function selectAnswer(){
 
 const questions = [
    {
-    question:"What can you use a can of Mountain Dew to dissolve?",
+    question:"What's 2+2?",
       
     answers:[
         {text: '4', correct: true},
-        {text: '22', correct: false}
+        {text: '22', correct: false},
+        {text: '11', correct: false},
+        {text: '99', correct: false},
       ]
-   }
+   },
 
 
 ]
